a4f8734877b133b7dca12a0f4b53606881a31599
krish9636singh <108384969+krish9636singh@users.noreply.github.com>
2022-12-18 09:21:35 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("API_ID", default=11008248, cast=int)
+API_HASH = config("API_HASH", default="355877e5f8d9c0294432230aa98b9038")
+BOT_TOKEN = config("BOT_TOKEN", default="5452506536:AAFbmp2u9WIw4eMzycJxQdBCkfni7jwduKI")
+SESSION = config("SESSION", default="BQB1zSLsKlQDn11-D3CMANay03O2yHnAy-x_9hEFBORi4liEwMTuo-uhkDl1etz09FPyWefKIQxWE_hY5MbGPLWJhppgJqcmLfDKD3UxAMeJfKJOaEE3ft85N6E9DFgggXMr-ySP4S8cVbYVeBWFgcJ162HvZ7I2M2Q4XliKX9MyoQEmocGQlCbKQfp938gJXH_0kMjt3NKk9ZiTpoVt7_ssO1LmTk9921NPTQIBdNz-BeBJ6eqd3V2-mhVZkQpDDnZI26xdNoADMgEotPanrNryoRYYGOZUJ0CLhnLFw2TQIuM7OZzWpx3594oLoy2A66T5nY-IUum66w97DZf5HXcIUHqL3AA")
+FORCESUB = config("FORCESUB", default="allmovieslinkhindi")
+AUTH = config("AUTH", default=1300886114, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
